be0b4f4220da6bef5084dd3a0dff3978f2d5daa9
Shenghao Li <li.sh0629@outlook.com>
2025-07-10 00:00:32 +0800

    可视按钮更改

--- Esaymap-vue/src/components/LayerManager.vue
+++ Esaymap-vue/src/components/LayerManager.vue
@@ -21,11 +21,17 @@ function toggleVisible(layer) {
       <div v-for="layer in layerStore.layers" :key="layer.id" class="list-item">
         <v-list-item class="d-flex align-center justify-space-between">
           <template #prepend>
-            <v-checkbox-btn
-              :model-value="layer.visible"
+            <v-btn
               @click.stop="toggleVisible(layer)"
-              density="compact"
-            />
+              icon
+              flat
+              plain
+              :ripple="false"
+              class="no-effect"
+            >
+              <v-icon>{{ layer.visible ? 'mdi-eye' : 'mdi-eye-off' }}</v-icon>
+            </v-btn>
+
           </template>
 
           <v-list-item-title class="text-subtitle-1">
@@ -60,10 +66,26 @@ function toggleVisible(layer) {
 }
 .list-item {
   border: 2px solid rgba(0, 0, 0, 0.2);
-  margin-top: 4px;
-  border-radius: 24px;
+  margin-top: 6px;
+  border-radius: 36px;
   /*box-shadow: 0px 2px 1px -1px var(--v-shadow-key-umbra-opacity, rgba(0, 0, 0, 0.2)), 0px 1px 1px 0px var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.14)), 0px 1px 3px 0px var(--v-shadow-key-ambient-opacity, rgba(0, 0, 0, 0.12));
 */
-  }
+}
+.v-btn--variant-elevated{
+ box-shadow: none;
+}
+.no-effect {
+  box-shadow: none !important;
+  background-color: transparent !important;
+}
+
+.no-effect:hover {
+  background-color: white !important;
+}
+
+.no-effect::before {
+  display: none !important; /* 取消点击涟漪效果 */
+  background: none !important;
+}
 
 </style>
